e38800b5cc71da02d8397265b2e61c77132a1eaf
Bram Vanroy <Bram.Vanroy@UGent.be>
2019-05-27 11:26:38 +0200

    Always return a dict of ms, sentences, and labels (maybe None)

--- pylt3/ml/rnn/LazyTextDataset.py
+++ pylt3/ml/rnn/LazyTextDataset.py
@@ -1,13 +1,16 @@
-from torch.utils.data import Dataset
-
 import linecache
+from pathlib import Path
+
+from torch.utils.data import Dataset
 
 
 class LazyTextDataset(Dataset):
-    def __init__(self, paths):
-        self.paths, self.labels_path = paths[:-1], paths[-1]
+    def __init__(self, paths_obj):
+        self.ms_p = str(Path(paths_obj['ms']).resolve()) if 'ms' in paths_obj else None
+        self.sentences_p = str(Path(paths_obj['sentences']).resolve()) if 'sentences' in paths_obj else None
+        self.labels_p = str(Path(paths_obj['labels']).resolve())
 
-        with open(self.labels_path, encoding='utf-8') as fhin:
+        with open(self.labels_p, encoding='utf-8') as fhin:
             lines = 0
             for line in fhin:
                 if line.strip() != '':
@@ -16,10 +19,13 @@ class LazyTextDataset(Dataset):
             self.num_entries = lines
 
     def __getitem__(self, idx):
-        data = [linecache.getline(p, idx + 1) for p in self.paths]
-        label = linecache.getline(self.labels_path, idx + 1)
+        d = {
+            'ms': linecache.getline(self.ms_p, idx + 1) if self.ms_p else None,
+            'sentences': linecache.getline(self.sentences_p, idx + 1) if self.sentences_p else None,
+            'labels': linecache.getline(self.labels_p, idx + 1)
+        }
 
-        return (*data, label)
+        return d
 
     def __len__(self):
         return self.num_entries
